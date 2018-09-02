3d953e81f100ba746dab9503e496974d85a0c168
Gusti Alfian M P <gustialfianmp@gmail.com>
2018-09-02 21:29:20 +0700

    step 0

--- steps/0.app.js
+++ steps/0.app.js
@@ -1,7 +1,11 @@
+// merujuk express
 const express = require('express');
 
+// instansiasi obejct express dan di simpan pada konstanta app
 const app = express();
 
+// contoh routing pada express
 app.get('/', (req, res) => res.send('Hello World'));
 
+// mendengarkan event yang terjadi pada localhost dengan port 3000
 app.listen(3000, () => console.log('listenig on localhos:3000'));
